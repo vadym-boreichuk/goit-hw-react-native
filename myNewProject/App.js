888c04f6aa336761f1a20e3a461384c8621e5b24
Борейчук Вадим <gefeste111@gmail.com>
2023-03-27 16:04:25 +0200

    start

---
+++ myNewProject/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <LoginScreen /> */}
+      <RegistrationScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
